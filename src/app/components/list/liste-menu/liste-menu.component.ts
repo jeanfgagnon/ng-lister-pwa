21725c7817febf3618fdcfb6cbcc25fd327cef87
JF Gagnon <lapuff@yahoo.com>
2022-05-02 16:31:00 -0400

    Enfin ca detecte le movement

--- src/app/components/list/liste-menu/liste-menu.component.ts
+++ src/app/components/list/liste-menu/liste-menu.component.ts
@@ -25,6 +25,9 @@ import { Subject } from 'rxjs';
 })
 export class ListeComponent implements OnInit, OnDestroy {
 
+  private swipeCoord?: [number, number];
+  private swipeTime?: number;
+
   public headers: ListHeader[] = [];
   public header: ListHeader;
   public selectedIdHeader = '';
@@ -76,6 +79,26 @@ export class ListeComponent implements OnInit, OnDestroy {
   }
 
   // event handlers
+
+  public swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+
+      if (duration < 1000 //
+        && Math.abs(direction[0]) > 30 // Long enough
+        && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { // Horizontal enough
+          const swipe = direction[0] < 0 ? 'next' : 'previous';
+          console.log(swipe);
+      }
+    }
+  }
 
   public headerSelected(idHeader: string): void {
     this.selectedIdHeader = idHeader;
